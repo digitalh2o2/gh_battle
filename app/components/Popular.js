e71f33a97938b4e43de3b16c878b9e5aa90f1116
Saul <digitalh2o@gmail.com>
2017-09-19 19:36:33 -0500

    Loading component complete and hosting will be done next

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 class SelectLanguage extends React.Component{
   render(){
@@ -103,7 +104,7 @@ class Popular extends React.Component{
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
-        {!this.state.repos ? <p>LOADING</p> :
+        {!this.state.repos ? <Loading />:
         <RepoGrid repos={this.state.repos} />}
       </div>
 
